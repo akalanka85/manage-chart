9b32fa8ea4d5906014e5371830a6efb3e49ca1da
Akalanka Silva <akalanka.silva@booknordics.com>
2024-08-09 14:12:50 +0300

    Change environmant variable to production

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   defaultChartWidth: 600,
-  apiUrl: 'http://localhost:3000/data',
+  apiUrl: 'https://83f6a4ab-298f-4169-b442-db41e5a5e438.mock.pstmn.io/data',
   chartTypes: [{name: 'Bar', value: 'bar'},
     {name: 'Column', value: 'column'},
     {name: 'Spline', value: 'spline'},
